2e122be046b4215d9ece44989690afde9bac0dd4
Jack_Du <51685343+ddx533534@users.noreply.github.com>
2025-12-12 19:33:31 +0800

    Update App.tsx
    add bug example

--- ReproducerApp/App.tsx
+++ ReproducerApp/App.tsx
@@ -1,45 +1,67 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React, { FC } from 'react';
+import { View, Text, StyleSheet, ViewStyle, TextStyle } from 'react-native';
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+const rem = (v: number): number => v;
 
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
+const TestApp: FC = () => {
   return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
-  );
-}
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
+    <View style={styles.root}>
+      <View style={styles.toastContent}>
+        <View style={styles.toastArrow} />
+        <Text style={styles.toastText}>Toast Content</Text>
+      </View>
     </View>
   );
+};
+
+export default TestApp;
+
+interface Styles {
+  root: ViewStyle;
+  toastContent: ViewStyle;
+  toastText: TextStyle;
+  toastArrow: ViewStyle;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+const styles = StyleSheet.create<Styles>({
+  root: {
+    marginTop: 200,
+    alignItems: 'center',
+  },
+
+  toastContent: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FF0000',
+    borderRadius: rem(4),
+    paddingLeft: rem(6),
+    paddingRight: rem(6),
+    height: rem(17),
+    position: 'relative',
+  },
+
+  toastText: {
+    color: '#FFFFFF',
+    fontSize: rem(10),
+    fontWeight: '400',
+    textAlign: 'center',
+  },
+
+  toastArrow: {
+    position: 'absolute',
+    right: rem(10),
+    bottom: 0,
+    marginBottom: -4,
+    width: 8,
+    height: 4,
+    borderLeftWidth: rem(4),
+    borderLeftColor: 'transparent',
+    borderRightWidth: rem(4),
+    borderRightColor: 'transparent',
+    borderTopWidth: rem(4),
+    borderTopColor: '#191919E6',
+    borderBottomWidth: 0,
+    borderBottomColor: 'transparent',
+    borderStyle: 'solid',
   },
 });
-
-export default App;
